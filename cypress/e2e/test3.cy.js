5ead2ae954982e1225f09f494402279cb19d004a
ninokhos <khositashvilinino2000@gmail.com>
2024-09-03 19:29:53 +0400

    test3

---
+++ cypress/e2e/test3.cy.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+  it('passes', () => {
+    //კალათაში დამატება და შემოწმება რომ პროდუქტი ნამდვილად დაემატა
+    cy.visit('https://testzootopia.loremipsum.ge/ka')
+     cy.get(':nth-child(5) > .swiper-container > .swiper-wrapper > .swiper-slide-active > h2 > a').click()
+     cy.get('.plus').click()
+     cy.get('.plus').click()
+     cy.get('.add-pro > figure').click()
+     cy.get('.menu-pop > [href="https://testzootopia.loremipsum.ge/ka/cart"] > p').click()
+  })
+})
---
+++ cypress/support/test3command.js
@@ -0,0 +1,8 @@
+Cypress.Commands.add("kalata", () =>{
+    cy.visit('https://testzootopia.loremipsum.ge/ka')
+     cy.get(':nth-child(5) > .swiper-container > .swiper-wrapper > .swiper-slide-active > h2 > a').click()
+     cy.get('.plus').click()
+     cy.get('.plus').click()
+     cy.get('.add-pro > figure').click()
+     cy.get('.menu-pop > [href="https://testzootopia.loremipsum.ge/ka/cart"] > p').click()
+})
